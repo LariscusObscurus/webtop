34de61a688e5b5c5d6664d23889ec595e0be902a
David Wolf <d.wolf@live.at>
2013-04-27 16:58:11 +0200

    fixed some errors in DbHandler

--- class/dbHandler.php
+++ class/dbHandler.php
@@ -3,9 +3,9 @@ class DbHandler
 {
 	private $connection;
 	
-	function __construct($host, $user, $password)
+	function __construct()
 	{
-		$this->connection = mysql_connect($host, $user, $password);
+		$this->connection = mysql_connect("localhost", "apache", "fingolfin");
 		if(!$connection) {
 			die('MySQL Fehler: '. mysql_error());
 		}
@@ -24,26 +24,34 @@ class DbHandler
 	
 	function addEntry($title, $link, $description)
 	{
-		$result = mysql_query("INSERT INTO rss (title, link, description, date) VALUES ('$title', '$link', '$description', NOW())", $this->connection);
-		if(!$result) {
-			die('MySQL Fehler: '. mysql_error());
-		}
+		$result = mysql_query("INSERT INTO rss (title, link, description, date) 
+			VALUES ('$title', '$link', '$description', NOW())", 
+			$this->connection);
+		return $result;
 	}
 	
 	function changeEntry($oldTitle, $oldLink, $oldDescription, $newTitle, $newLink, $newDescription)
 	{
-		$result = mysql_query("UPDATE rss SET title = '$newTitle', link = '$newLink', description = '$newDescription', date = NOW() WHERE title = '$oldTitle' AND link = '$oldLink' AND description = '$oldDescription';", $this->connection);
-		if(!$result) {
-			die('MySQL Fehler: '. mysql_error());
-		}
+		$result = mysql_query("UPDATE rss SET 
+			title = '$newTitle', 
+			link = '$newLink', 
+			description = '$newDescription', 
+			date = NOW() 
+		WHERE title = '$oldTitle' AND 
+			link = '$oldLink' AND 
+			description = '$oldDescription';", 
+			$this->connection);
+		return $result;
 	}
 	
 	function deleteEntry($title, $link, $description)
 	{
-		$result = mysql_query("DELETE FROM rss WHERE title = '$title' AND link = '$link' AND description = '$description';");
-		if(!$result) {
-			die('MySQL Fehler: '. mysql_error());
-		}
+		$result = mysql_query("DELETE FROM rss WHERE 
+			title = '$title' AND 
+			link = '$link' AND 
+			description = '$description';",
+			$this->description);
+		return $result;
 	}
 }
 ?>
