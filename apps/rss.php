30d94ac4efc0f259a03494dd06e78a6f87d4c4e5
David Wolf <d.wolf@live.at>
2013-04-28 15:45:16 +0200

    last changes

--- apps/rss.php
+++ apps/rss.php
@@ -1,7 +1,9 @@
 <form action="" method="post" enctype="multipart/form-data">
 	<label for="rssLink">Datei: </label>
 	<input id="rssLink" type="text" name="LinkRSS" onclick="onClickRssText(event)"/>
-	<input id="rssSend" type="button" name="SendRSS" value="SendRSS" onclick="onClickRssSend(event)"/>
+	<input id="rssSend" type="button" name="SendRSS" value="SendRSS" onclick="onClickRssSend(event)"/><br/>
+	<!--<iframe src="../pages/atom.php" style="width: 600px; height: 400px; margin-top: 8px;">
+	</iframe>-->
 </form>
 <?php
 ?>
