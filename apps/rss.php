c16e48aff749854db4532652711aeed3368aefe6
David Wolf <d.wolf@live.at>
2013-04-28 15:49:06 +0200

    div in rss.php

--- apps/rss.php
+++ apps/rss.php
@@ -2,8 +2,8 @@
 	<label for="rssLink">Datei: </label>
 	<input id="rssLink" type="text" name="LinkRSS" onclick="onClickRssText(event)"/>
 	<input id="rssSend" type="button" name="SendRSS" value="SendRSS" onclick="onClickRssSend(event)"/><br/>
-	<!--<iframe src="../pages/atom.php" style="width: 600px; height: 400px; margin-top: 8px;">
-	</iframe>-->
+	<!--<iframe src="../pages/atom.php" style="width: 600px; height: 400px; margin-top: 8px;"></iframe>-->
+	<div xmlns="http://www.w3.org/1999/xhtml" style="width: 600px; height: 400px; margin-top: 8px; overflow-y: scroll; overflow-x: hidden;"></div>
 </form>
 <?php
 ?>
